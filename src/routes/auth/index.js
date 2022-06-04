a4c9cb3d922f5eb43ab4970b4172e42b913799d8
Ezumah <jerozeek2@gmail.com>
2022-06-04 13:25:29 +0100

    naming_route

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -3,16 +3,16 @@ const verifyToken = require('../../middleware/verifyToken');
 const throttle = require('../../middleware/throttle');
 
 // Importing the auth controller
-const authController = require('../../Controllers/Api/Auth/AuthController');
+const AuthController = require('../../Controllers/Api/Auth/AuthController');
 
 
-router.post('/login', [throttle(60, 1)], authController.login);
-router.post('/register', [throttle(60, 1)], authController.register);
-router.post('/verify-account', [throttle(60, 1), verifyToken], authController.verifyAccount);
-router.get('/resend-verification-otp', [throttle(60, 1), verifyToken], authController.resendVerificationOTP);
-router.post('/forgot-password', [throttle(60, 1)], authController.passwordResetRequest);
-router.post('/verify-password-reset-otp', [throttle(60, 1), verifyToken], authController.verifyPasswordResetOTP);
-router.get('/resend-password-reset-otp', [throttle(60, 1)], authController.resendPasswordResetOTP);
-router.post('/reset-password', [throttle(60, 1), verifyToken], authController.resetPassword);
+router.post('/login', [throttle(60, 1)], AuthController.login);
+router.post('/register', [throttle(60, 1)], AuthController.register);
+router.post('/verify-account', [throttle(60, 1), verifyToken], AuthController.verifyAccount);
+router.get('/resend-verification-otp', [throttle(60, 1), verifyToken], AuthController.resendVerificationOTP);
+router.post('/forgot-password', [throttle(60, 1)], AuthController.passwordResetRequest);
+router.post('/verify-password-reset-otp', [throttle(60, 1), verifyToken], AuthController.verifyPasswordResetOTP);
+router.get('/resend-password-reset-otp', [throttle(60, 1)], AuthController.resendPasswordResetOTP);
+router.post('/reset-password', [throttle(60, 1), verifyToken], AuthController.resetPassword);
 
 module.exports = router;
